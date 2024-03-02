4b16c055d950d40b8964ec671faada87e5cd42c5
Ivan Balakin <ivanbal1821@yandex.ru>
2024-03-02 21:10:47 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,85 @@
+import React, { useState } from 'react';
+import { MenuFoldOutlined, MenuUnfoldOutlined, UploadOutlined, UserOutlined, VideoCameraOutlined } from '@ant-design/icons';
+import { Routes, Route, Navigate, Link} from 'react-router-dom';
+import { Layout, Menu, Button, theme } from 'antd';
+
+
+import Home from './pages/Home';
+import Storage from './pages/Storage';
+
+import './styles/app.sass';
+
+
+const { Header, Sider, Content } = Layout;
+
+const App = () => {
+    const [collapsed, setCollapsed] = useState(false);
+    const {
+        token: { colorBgContainer, borderRadiusLG },
+    } = theme.useToken();
+
+    return (
+        <Layout className='app'>
+            <Sider trigger={null} collapsible collapsed={collapsed} >
+                <div className="demo-logo-vertical" />
+                <Menu
+                    theme="dark"
+                    mode="inline"
+                    defaultSelectedKeys={['1']}
+                    items={[
+                        {
+                            key: '1',
+                            icon: <UserOutlined />,
+                            label: <Link to="/home">Главная</Link>,
+                        },
+                        {
+                            key: '2',
+                            icon: <VideoCameraOutlined />,
+                            label: <Link to="/storage">Склад</Link>,
+                        },
+                        {
+                            key: '3',
+                            icon: <UploadOutlined />,
+                            label: 'nav 3',
+                        },
+                    ]}
+                />
+            </Sider>
+            <Layout>
+                <Header
+                    style={{
+                        padding: 0,
+                        background: colorBgContainer,
+                    }}
+                >
+                    <Button
+                        type="text"
+                        icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+                        onClick={() => setCollapsed(!collapsed)}
+                        style={{
+                            fontSize: '16px',
+                            width: 64,
+                            height: 64,
+                        }}
+                    />
+                </Header>
+                <Content
+                    style={{
+                        margin: '24px 16px',
+                        padding: 24,
+                        minHeight: 280,
+                        background: colorBgContainer,
+                        borderRadius: borderRadiusLG,
+                    }}
+                >
+                    <Routes>
+                        <Route path="/" element={<Home/>} />
+                        <Route path="/storage" element={<Storage/>} />
+                        <Route path="*" element={<Navigate to="/" replace />}/>
+                    </Routes>
+                </Content>
+            </Layout>
+        </Layout>
+    );
+};
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,44 @@
+import { useSelector, useDispatch } from 'react-redux'
+import { decrement, increment, incrementByAmount, decrementByAmount } from '../store/mainSlice'
+
+
+export default function Home() {
+  const count = useSelector((state) => state.main.value)
+  const dispatch = useDispatch()
+
+  return (
+    <div>
+      <div>
+        <button
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </button>
+        <span>{count}</span>
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </button>
+      </div>
+
+      <div>
+        <button
+          aria-label="Increment value"
+          onClick={() => dispatch(incrementByAmount(2))}
+        >
+          IncrementByAmount
+        </button>
+        <span>{count}</span>
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrementByAmount(2))}
+        >
+          DecrementByAmount
+        </button>
+      </div>
+    </div>
+  )
+}
